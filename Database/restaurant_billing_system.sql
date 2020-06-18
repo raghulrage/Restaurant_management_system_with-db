56234736207b93ad17b1d8a46a01a91bb4da9957
Raghul T <raghulrage@gmail.com>
2020-06-18 12:45:13 +0530

    Updated database.sql

--- Database/restaurant_billing_system.sql
+++ Database/restaurant_billing_system.sql
@@ -1,204 +1,137 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 12, 2020 at 08:26 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: restaurant_billing_system
+-- ------------------------------------------------------
+-- Server version	10.3.22-MariaDB-1ubuntu1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `restaurant_billing_system`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `customer`
 --
 
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customer` (
-  `customer_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `customer_name` varchar(50) DEFAULT NULL,
   `mobile_no` varchar(20) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customer_id`, `email`, `customer_name`, `mobile_no`, `address`, `password`) VALUES
-(8, 'raghulrage@gmail.com', 'Raghul T', '8428616811', 'coimbatore', 'raghul'),
-(10, 'dhanya@gmail.com', 'Dhanya T', '1234567890', 'coimbatore', 'dhanya'),
-(11, 'thangavel@gmail.com', 'Thangavel P', '123467890', 'Coimbatore', 'thangavel'),
-(12, 'jeeva@gmail.com', 'Jeeva T', '9894042900', 'Coimbatore', 'jeeva');
-
--- --------------------------------------------------------
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (8,'raghulrage@gmail.com','Raghul T','8428616811','coimbatore','cb2b9c90c00a8dbe96a920edbf680f936f7517d9ddd1a5a6686ac758a52c6081'),(10,'dhanya@gmail.com','Dhanya T','1234567890','coimbatore','53fb4df1f62b3094a032082446ea369d213e48c2f83ea96384b15141bf29b3ea'),(11,'thangavel@gmail.com','Thangavel P','123467890','Coimbatore','fbb06f90c3f4c0ee4d2e9e88318a715fd07ba7066d1b6c248ca0f3e2c1adbb8e'),(12,'jeeva@gmail.com','Jeeva T','9894042900','Coimbatore','0e0aac11b76e6ba2e2850268d30529db9ee86750254cafa9aab7812bb0c59ee5'),(13,'dhanush@gmail.com','Dhanush','1234567890','Chennai, Tamil Nadu','22fe491ae20e1d05c9c00047c85e72697edcf63114d6618d553ab55577d9a872');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `food`
 --
 
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `food` (
-  `food_id` int(11) NOT NULL,
+  `food_id` int(11) NOT NULL AUTO_INCREMENT,
   `food_name` varchar(50) DEFAULT NULL,
-  `price` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `price` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`food_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `food`
 --
 
-INSERT INTO `food` (`food_id`, `food_name`, `price`) VALUES
-(3, 'Parotta', '15'),
-(4, 'Dosa', '10'),
-(5, 'Chappathi', '20'),
-(6, 'Idly', '5'),
-(7, 'Pongal', '25'),
-(8, 'Poori', '15'),
-(9, 'Meals', '50'),
-(10, 'Fried Rice', '45'),
-(11, 'Shawarma', '40'),
-(12, 'Pizza', '80'),
-(13, 'Burger', '45'),
-(14, 'Biriyani', '90');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `total` varchar(10) DEFAULT NULL,
-  `date` varchar(50) NOT NULL 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`order_id`, `customer_id`, `total`, `date`) VALUES
-(8, 8, '70', '2020-06-11 22:31:56'),
-(9, 8, '105', '2020-06-11 22:33:21'),
-(10, 8, '180', '2020-06-11 22:34:27'),
-(11, 10, '105', '2020-06-11 22:50:00'),
-(12, 8, '30', '2020-06-11 22:57:19');
-
--- --------------------------------------------------------
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (3,'Parotta','15'),(4,'Dosa','10'),(5,'Chappathi','20'),(6,'Idly','5'),(7,'Pongal','25'),(8,'Poori','15'),(9,'Meals','50'),(10,'Fried Rice','45'),(11,'Shawarma','40'),(12,'Pizza','80'),(13,'Burger','45'),(14,'Biriyani','90');
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `order_details`
 --
 
+DROP TABLE IF EXISTS `order_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `order_details` (
   `order_id` int(11) DEFAULT NULL,
   `food_id` int(11) DEFAULT NULL,
-  `quantity` varchar(10) DEFAULT NULL
+  `quantity` varchar(10) DEFAULT NULL,
+  KEY `order_id` (`order_id`),
+  KEY `food_id` (`food_id`),
+  CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `order_details`
 --
 
-INSERT INTO `order_details` (`order_id`, `food_id`, `quantity`) VALUES
-(8, 3, '2'),
-(8, 11, '1'),
-(9, 3, '2'),
-(9, 7, '3'),
-(10, 14, '1'),
-(10, 10, '2'),
-(11, 3, '2'),
-(11, 7, '3'),
-(12, 3, '2');
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+INSERT INTO `order_details` VALUES (8,3,'2'),(8,11,'1'),(9,3,'2'),(9,7,'3'),(10,14,'1'),(10,10,'2'),(11,3,'2'),(11,7,'3'),(12,3,'2'),(13,3,'2'),(13,8,'3');
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for dumped tables
+-- Table structure for table `orders`
 --
 
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD UNIQUE KEY `email` (`email`);
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `total` varchar(10) DEFAULT NULL,
+  `date` varchar(50) NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for table `food`
---
-ALTER TABLE `food`
-  ADD PRIMARY KEY (`food_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `food_id` (`food_id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `orders`
 --
 
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (8,8,'70','2020-06-11 22:31:56'),(9,8,'105','2020-06-11 22:33:21'),(10,8,'180','2020-06-11 22:34:27'),(11,10,'105','2020-06-11 22:50:00'),(12,8,'30','2020-06-11 22:57:19'),(13,10,'75','2020-06-17 15:51:55');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- AUTO_INCREMENT for table `food`
---
-ALTER TABLE `food`
-  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`);
-
---
--- Constraints for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-18 12:44:12
